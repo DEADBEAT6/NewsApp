bca406df57dcbb5e3286f0fc8dc4e606c3356454
Raj Aryan Jain <107569541+DEADBEAT6@users.noreply.github.com>
2024-07-08 17:51:33 +0530

    Initial commit

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val USER_SETTINGS = "user_settings"
+    const val APP_ENTRY = "app_entry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "11b75f6f4e214cb4a9414fab55a7f42b"
+
+}
